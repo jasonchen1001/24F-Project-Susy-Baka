2d5b409d4ff154303a0643d17b97c882b0fb32e2
陈一舟 <150562284+jasonchen1001@users.noreply.github.com>
2024-11-27 17:47:27 -0500

    modify student_routes

--- api/backend/student/student_routes.py
+++ api/backend/student/student_routes.py
@@ -1,3 +1,7 @@
+########################################################
+# Student blueprint
+########################################################
+
 from flask import Blueprint, request, jsonify, make_response, current_app
 from backend.db_connection import db
 
