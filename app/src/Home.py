5e87777ef59e826c1404f8e160ae319843e88d71
陈一舟 <150562284+jasonchen1001@users.noreply.github.com>
2024-11-27 18:25:04 -0500

    Update main page with user roles and styling

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,77 +1,88 @@
 ##################################################
-# This is the main/entry-point file for the 
-# sample application for your project
+# Main entry-point file for Student Career System
 ##################################################
-
-# Set up basic logging infrastructure
 import logging
 logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# import the main streamlit library as well
-# as SideBarLinks function from src/modules folder
 import streamlit as st
 from modules.nav import SideBarLinks
 
-# streamlit supports reguarl and wide layout (how the controls
-# are organized/displayed on the screen).
-st.set_page_config(layout = 'wide')
-
-# If a user is at this page, we assume they are not 
-# authenticated.  So we change the 'authenticated' value
-# in the streamlit session_state to false. 
+st.set_page_config(layout='wide')
 st.session_state['authenticated'] = False
-
-# Use the SideBarLinks function from src/modules/nav.py to control
-# the links displayed on the left-side panel. 
-# IMPORTANT: ensure src/.streamlit/config.toml sets
-# showSidebarNavigation = false in the [client] section
 SideBarLinks(show_home=True)
 
-# ***************************************************
-#    The major content of this page
-# ***************************************************
-
-# set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('CS 3200 Sample Semester Project App')
+st.title('Student Career Management System')
 st.write('\n\n')
-st.write('### HI! As which user would you like to log in?')
+st.write('### Welcome! Please select your role to continue:')
 
-# For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user 
-# can click to MIMIC logging in as that mock user. 
-
-if st.button("Act as John, a Political Strategy Advisor", 
-            type = 'primary', 
+# Student Role (Alice Johnson)
+if st.button("Login as Alice Johnson (Student)", 
+            type='primary',
             use_container_width=True):
-    # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
-    # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
-    # we add the first name of the user (so it can be displayed on 
-    # subsequent pages). 
+    st.session_state['role'] = 'Student'
+    st.session_state['first_name'] = 'Alice'
+    st.session_state['user_id'] = 1
+    logger.info("Logging in as Student")
+    st.switch_page('pages/00_Student_Home.py')
+
+# School Admin Role (William Smith)
+if st.button("Login as William Smith (School Administrator)",
+            type='primary',
+            use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'School_Admin'
+    st.session_state['first_name'] = 'William'
+    st.session_state['admin_id'] = 1
+    logger.info("Logging in as School Administrator")
+    st.switch_page('pages/10_Admin_Home.py')
+
+# HR Manager Role (John Anderson)
+if st.button("Login as John Anderson (HR Manager)",
+            type='primary',
+            use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'HR_Manager'
     st.session_state['first_name'] = 'John'
-    # finally, we ask streamlit to switch to another page, in this case, the 
-    # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+    st.session_state['hr_id'] = 1
+    logger.info("Logging in as HR Manager")
+    st.switch_page('pages/20_HR_Home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
-            type = 'primary', 
+# Maintenance Staff Role (Thomas Anderson)
+if st.button("Login as Thomas Anderson (Maintenance Staff)",
+            type='primary',
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+    st.session_state['role'] = 'Maintenance_Staff'
+    st.session_state['first_name'] = 'Thomas'
+    st.session_state['staff_id'] = 1
+    logger.info("Logging in as Maintenance Staff")
+    st.switch_page('pages/30_Maintenance_Home.py')
 
-if st.button('Act as System Administrator', 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'administrator'
-    st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
+# Add system description
+st.markdown("""
+---
+### About the Student Career Management System
 
+This integrated platform helps:
+* **Students** manage resumes and track internship applications
+* **School Administrators** oversee academic records and approve co-op experiences
+* **HR Managers** post internship opportunities and review applications
+* **Maintenance Staff** ensure system reliability and data integrity
 
+Choose your role above to access role-specific features.
 
+""")
+
+# Add some visual styling
+st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #0483ee;
+    color: white;
+    margin-bottom: 10px;
+}
+</style>
+""", unsafe_allow_html=True)
